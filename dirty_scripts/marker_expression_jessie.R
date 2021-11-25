eb0fffcb55ebdba61fe0359a395cdb4bbe237607
Chieh-Yu Lee <chiehyulee97@gmail.com>
2021-11-25 16:21:03 +0100

    add modified splits
    split2 for EPCAM
    split3 for PECAM1
    modified for new mean value

--- dirty_scripts/marker_expression_jessie.R
+++ dirty_scripts/marker_expression_jessie.R
@@ -9,14 +9,20 @@ library(gridExtra)
 wd = "/omics/groups/OE0219/internal/Jessie_2021/P01.digital_sorter"
 setwd(wd)
 ob = readRDS("/omics/groups/OE0219/internal/Jessie_2021/P01.digital_sorter/rawdata/song_2019.rds")
-normal = read.csv("/omics/groups/OE0219/internal/Jessie_2021/P01.digital_sorter/procdata/maker_gene_expression_in_normal_lung.csv")
-#ob@meta.data$disease[!(ob@meta.data$disease=="adjacent normal"|ob@meta.data$disease=="normal"|ob@meta.data$disease=="nsclc")]="nsclc_meta"
-table(ob@meta.data$disease)
-#adjacent normal 6598 #nsclc 4883
-table(ob@meta.data$donor)
-#P1_Normal  P1_Tumor P2_Normal  P2_Tumor P3_Normal  P3_Tumor P4_Normal  P4_Tumor 
-#  2495      1832      1409      1300       667       328      2027      1423 
+#ob = readRDS("/omics/groups/OE0219/internal/MJMC/P01_NSCLC/P01.2_enriched_cell_components/analysis/azimuth-meta-analysis/human_lung/cellxgene/lung_mapped_cellxgene.rds")
 
+## deal with normal table
+#normal = read.csv("/omics/groups/OE0219/internal/Jessie_2021/P01.digital_sorter/procdata/maker_gene_expression_in_normal_lung.csv")
+#normal_EPCAM <- readRDS("dirty_scripts/normal_mean_EPCAM.rds")
+#normal_EPCAM <- normal_EPCAM[,c(1,3)]
+#normal2 <- merge(normal, normal_EPCAM, by.x = "Cell.type", by.y = "Group.1", all.x =T)
+#normal_PECAM1 <- readRDS("dirty_scripts/normal_mean_PECAM1.rds")
+#normal_PECAM1 <- normal_PECAM1[,c(1,3)]
+#normal3 <- merge(normal2, normal_PECAM1, by.x = "Cell.type", by.y = "Group.1", all.x =T)
+#normal3 <- normal3[,c(1,3,4,7,8)]
+#colnames(normal3) <- c("Cell.type","source","PTPRC","EPCAM","PECAM1")
+#saveRDS(normal3,"procdata/maker_gene_expression_in_normal_lung_jessie.RDS")
+normal <- readRDS("procdata/maker_gene_expression_in_normal_lung_jessie.RDS")
 
 cols = c("steelblue","darkred","gold","coral2")
 
@@ -28,8 +34,9 @@ m1 =VlnPlot(ob, features, split.by = "disease", group.by = "annotation.l2", cols
 m1
 
 d = m1[[1]]$data
-
-normal_PTPRC <- mean(normal$PTPRC, na.rm = T)
+cell <- as.character(unique(sort(ob@meta.data[["annotation.l2"]])))
+normal_sub <- normal[normal$Cell.type %in% cell,]
+normal_PTPRC <- mean(normal_sub$PTPRC, na.rm = T)
 
 PTPRC_group <- c()
 for(i in 1:length(normal$PTPRC)){
@@ -48,20 +55,21 @@ ob <- AddMetaData(object = ob, metadata = meta, col.name = "split1.1")
 
 
 # EPCAM ####
-ob_split <- SplitObject(ob, split.by = "split1.1")
-ob_split_PTPRC0 = ob_split$`PTPRC-`
+ob2 <- SplitObject(ob, split.by = "split1.1")
+ob2 = ob2$`PTPRC-`
 
 features=c("EPCAM")
-m1 =VlnPlot(ob_split_PTPRC0, features, split.by = "disease", group.by = "annotation.l2", 
+m1 =VlnPlot(ob2, features, split.by = "disease", group.by = "annotation.l2", 
             cols = cols,sort = TRUE,pt.size = 0, combine = FALSE)
 m1
 d = m1[[1]]$data
-cell <- as.character(unique(sort(ob_split_PTPRC0@meta.data[["annotation.l2"]])))
+cell <- as.character(unique(sort(ob2@meta.data[["annotation.l2"]])))
 normal_sub <- normal[normal$Cell.type %in% cell,]
-normal_EPCAM <-  mean(normal_sub$EPCAM)
+normal_EPCAM <-  mean(normal_sub$EPCAM) #median not mean #use mean: club cell would be EPCAM-
 EPCAM_group <- c()
 for(i in 1:length(normal$EPCAM)){
-  if(normal$EPCAM[i] >=normal_EPCAM){EPCAM_group <- append(EPCAM_group,"EPCAM+")}
+  if (is.na(normal$EPCAM[i])) {EPCAM_group <- append(EPCAM_group,"NA")}
+  else if(normal$EPCAM[i] >=normal_EPCAM){EPCAM_group <- append(EPCAM_group,"EPCAM+")}
   else{EPCAM_group <- append(EPCAM_group,"EPCAM-")}
 }
 
@@ -73,27 +81,29 @@ meta = ob$annotation.l2
 meta[meta %in% set1] = paste0(features, "+")
 meta[meta %in% set2] = paste0(features, "-")
 
-ob2 <- AddMetaData(object = ob, metadata = meta, col.name = "split2")
-ob_split = SplitObject(ob, split.by = "disease")
+ob <- AddMetaData(object = ob, metadata = meta, col.name = "split2")
+ob2 <- AddMetaData(object = ob2, metadata = meta, col.name = "split2")
+
 
 
 
 ## Level 3
 ob3 <- SplitObject(ob2, split.by = "split2")
-ob_split_EPCAM0 = ob3$`EPCAM-`
+ob3 = ob3$`EPCAM-`
 
 features=c("PECAM1")
-m1 =VlnPlot(ob_split_EPCAM0, features, split.by = "disease", group.by = "annotation.l2", 
+m1 =VlnPlot(ob3, features, split.by = "disease", group.by = "annotation.l2", 
             cols = cols,sort = TRUE,pt.size = 0, combine = FALSE)
 
 m1
 d = m1[[1]]$data
 cell <- as.character(unique(sort(d$ident)))
 normal_sub <- normal[normal$Cell.type %in% cell,]
-normal_PECAM1 <-  mean(normal_sub$PECAM1)
+normal_PECAM1 <-  median(normal_sub$PECAM1) #median instead of mean
 PECAM1_group <- c()
 for(i in 1:length(normal$PECAM1)){
-  if(normal$PECAM1[i] >=normal_PECAM1){PECAM1_group <- append(PECAM1_group,"PECAM1+")}
+  if (is.na(normal$PECAM1[i])) {PECAM1_group <- append(PECAM1_group,"NA")}
+  else if(normal$PECAM1[i] >=normal_PECAM1){PECAM1_group <- append(PECAM1_group,"PECAM1+")}
   else{PECAM1_group <- append(PECAM1_group,"PECAM1-")}
 }
 
@@ -105,27 +115,11 @@ meta = ob$annotation.l2
 meta[meta %in% set1] = paste0(features, "+")
 meta[meta %in% set2] = paste0(features, "-")
 
-ob3 <- AddMetaData(object = ob2, metadata = meta, col.name = "split3")
+ob <- AddMetaData(object = ob, metadata = meta, col.name = "split3")
 
-saveRDS(ob3,"/omics/groups/OE0219/internal/Jessie_2021/P01.digital_sorter/rawdata/song_2019_addsplits.rds")
+saveRDS(ob,"/omics/groups/OE0219/internal/Jessie_2021/P01.digital_sorter/rawdata/song_2019_addsplits.rds")
 
 normal$X <- NULL
 
 write.csv(normal,"procdata/maker_gene_expression_in_normal_lung_add_groups.csv")
-####### CL ##############
-CL <- read.csv("procdata/CL_cell_names_lee.csv")
-CL <- CL[,3]
-CL <- unique(sort(CL))
-anno1 <- ob@meta.data[["annotation.l1"]]
-anno1 <- unique(sort(anno1))
 
-anno2 <- ob@meta.data[["annotation.l2"]]
-anno2 <- unique(sort(anno2))
-
-n <- max(length(anno1), length(anno2),length(CL))
-length(anno1) <- n                      
-length(anno2) <- n
-
-cell_anno <- cbind(CL,anno1,anno2)
-colnames(cell_anno) <- c("Cell ontology","annotation.l1","annotation.l2")
-write.csv(cell_anno,"dirty_scripts/compare_cell_anno.csv")
